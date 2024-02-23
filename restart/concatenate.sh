6d4281dfac9badffe17ea594cbc7dc643708c0a5
shao <shaoyu.ts@gmail.com>
2024-02-24 04:29:52 +0800

    feat(restart): concatenate restart folder and create ctl

---
+++ restart/concatenate.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+vvmPath="/data/mog/VVM/DATA/"
+#/data/mog/VVM/DATA/RRCE_3km_f10_1
+
+outVvmPath="/data/C.shaoyu/rrce/vvm/"
+
+typeList="C.Surface L.Diag L.Dynamic L.Radiation L.Thermodynamic"
+expList="RRCE_3km_f00 RRCE_3km_f05 RRCE_3km_f10 RRCE_3km_f15 RCE_300K_3km_f0 RCE_300K_3km_f05"
+
+
+for exp in ${expList};do
+  echo "----- ${exp} -----"
+
+  dir="${vvmPath}/${exp}/"
+  if [ ! -d ${dir} ];then
+    echo "can't found the case '${exp}' ... skip"
+    continue
+  fi
+  out="${outVvmPath}/${exp}/"
+  rm -rf ${out}
+
+  mkdir -p ${out}
+  ln -sf ${dir}/TOPO.nc      ${out}/
+  ln -sf ${dir}/fort.98      ${out}/
+  ln -sf ${dir}/vvm.setup    ${out}/
+
+  n=$(ls -d ${vvmPath}/${exp}_*|wc -l)
+  if [ ${n} -le 0 ]; then
+    ln -sf ${dir}/archive     ${out}/
+    echo "no restart folder ..."
+    bash make_ctl.sh ${out}
+    continue
+  fi
+
+  echo "There are ${n} folder to be merged ..."
+  mkdir ${out}/archive
+  ln -sf ${dir}/archive/*.nc ${out}/archive/
+  for i in $(seq 1 ${n}) ;do
+    echo "process the restart_${i} ..."
+    dir="${vvmPath}/${exp}_${i}/"
+    if [ ! -d ${dir} ];then
+      echo "can't found the restart folder: ${dir} ..."
+      echo "please check the rule of the restart folder ... skip"
+      break
+    fi
+
+    if [ ${i} -eq 1 ]; then
+      pre_exp=${exp}
+    else
+      i0=$(echo ${i}-1|bc)
+      pre_exp=${exp}_${i0}
+    fi
+
+    idx0="notfound"
+    if [ -f ${dir}/restart.log ];then
+      cp -r "${dir}/restart.log" ${out}/restart_${i}.log
+      idx0=$(head -n 1 ${dir}/restart.log|rev|cut -d" " -f1|rev)
+    else
+      idx0=$(grep "${pre_exp}.L.Thermodynamic" \
+            ${vvmPath}/${exp}_${i}/CODE/ini_3d_module.F|\
+            rev|cut -d"-" -f1|rev|cut -c1-6)
+      echo "restart timestep ... ${idx0} 
+            from ${vvmPath}/${exp}_${i}/CODE/ini_3d_module.F" > ${out}/restart_${i}.log
+    fi
+
+    if [ "${idx0}" == "notfound" ]; then
+      echo "can't found the restart timestep ... skip"
+      break
+    fi
+   
+    if [ ${i} -eq 1 ]; then
+      lastnum=${idx0}
+    else
+      dum=$(ls ${vvmPath}/${pre_exp}/archive/${pre_exp}.C.Surface*.nc|wc -l)
+      lastnum=$(echo "${lastnum}-(${dum}-1-${idx0})"|bc)
+    fi
+ 
+    nfile=$(ls ${dir}/archive/${exp}_${i}.C.Surface*.nc|wc -l)
+    nfile=$(echo ${nfile}-1|bc)
+    echo "restart init_timestep is ${idx0} and the total number is ${nfile}"
+    for inc in $(seq 1 ${nfile});do
+      inc=$(printf "%06d" ${inc})
+      idx=$(echo ${lastnum}+${inc}|bc)
+      idx=$(printf "%06d" ${idx})
+      #echo "${inc}-->${idx}"
+      for vtype in ${typeList};do
+        #echo ${dir}/archive/${exp}_${i}.${vtype}-${inc}.nc
+        ln -sf ${dir}/archive/${exp}_${i}.${vtype}-${inc}.nc \
+               ${out}/archive/${exp}.${vtype}-${idx}.nc
+      done
+    done # inc
+    lastnum=${lastnum}+${nfile}
+
+  done # restart folder (i)
+
+  bash make_ctl.sh ${out}
+  
+done #exp
---
+++ restart/make_ctl.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+rundir="/data/C.shaoyu/rrce/vvm/RRCE_3km_f00"
+rindir=${1}
+ncdir="${rundir}/archive"
+outdir="${rundir}/gs_ctl_files"
+mkdir -p ${outdir}
+
+declare -A vtab
+vtab[time]='t'
+vtab[lon]='x'
+vtab[lat]='y'
+vtab[lev]='z'
+
+# ----- number of data type and ncheader
+dtype_list=""
+for dum in $(ls ${ncdir}/*000000.nc);do
+  dum=$(echo ${dum}|rev|cut -d"/" -f1|rev|cut -d"-" -f1)
+  dum0=$(echo ${dum}|rev|cut -d"." -f2|rev)  #L
+  dum1=$(echo ${dum}|rev|cut -d"." -f1|rev)  #Thermodynamic
+  ncheader=$(echo ${dum}|rev|cut -d"." -f3|rev)  #RRCE_3km_f00
+  dtype_list="${dtype_list} ${dum0}.${dum1}"
+done
+
+# ----- dimension
+nt=$(ls ${ncdir}/*.${dum0}.${dum1}-*.nc|wc -l)
+nx=$(grep "zonal_dimension" ${rundir}/vvm.setup|cut -d"'" -f2|cut -d"/" -f3)
+ny=$(grep "merid_dimension" ${rundir}/vvm.setup|cut -d"'" -f2|cut -d"/" -f3)
+nz=$(grep "vert_dimension" ${rundir}/vvm.setup|cut -d"'" -f2|cut -d"/" -f3)
+
+# get dt
+outfreq=$(grep "NXSAVG=" ${rundir}/vvm.setup |cut -d"," -f2|cut -d"=" -f2)
+dt=$(grep "DT=" ${rundir}/vvm.setup |cut -d"," -f5|cut -d"=" -f2)
+deltatime=$(echo "scale=0;${outfreq}*${dt}/60"|bc)
+echo ${deltatime} min
+
+# ----- get z level
+dum=$(grep -n "ZT(K)" ${rundir}/fort.98|cut -d":" -f1)
+dum=$(echo ${dum}+${nz}+1|bc)
+table=$(cat ${rundir}/fort.98 |head -n ${dum}|tail -n ${nz})
+zlist=""
+for i in $(seq ${nz});do
+  idx=$(echo "3+(${i}-1)*5"|bc)
+  dum=$(echo ${table}|cut -d" " -f${idx})
+  dum=$(echo "scale=3;(${dum}/1000.)"|bc)
+  zlist="${zlist} ${dum}"
+done
+
+for dtype in ${dtype_list};do
+  type0=$(echo ${dtype}|cut -d. -f1)
+  type1=$(echo ${dtype}|cut -d. -f2)
+
+  if [ ${type0} == "L" ];then
+    outz=${zlist}
+    outnz=${nz}
+    outnz1=${nz}
+  else
+    outz=1000
+    outnz=0
+    outnz1=1
+  fi
+
+  # ------ get varables
+  table=$(ncdump -h ${ncdir}/${ncheader}.${dtype}-000000.nc|grep "float")
+  table=${table// /.}
+  varstring=""
+  nvar=0
+  for dum in ${table};do
+    vname=$(echo ${dum}|cut -d. -f2|cut -d"(" -f1)
+    if [ "${vname}" == "time" ]; then continue; fi
+    nvar=$((${nvar}+1))
+    dim=$(echo ${dum}|cut -d"(" -f2|cut -d")" -f1)
+    dimstr=""
+    for v in ${dim//,./ };do
+      dimstr="${dimstr},${vtab[$v]}"
+    done
+    dimstr=$(echo ${dimstr}|cut -c2-10000)
+    varstring="${varstring}\n${vname}=>${vname} ${outnz} ${dimstr} xxx"
+  done
+  echo ${dtype} ${nvar}
+  
+  string="
+  DSET ^../archive/${ncheader}.${dtype}-%tm6.nc\n
+  DTYPE netcdf\n
+  OPTIONS template\n
+  TITLE ${dtype} variables\n
+  UNDEF 99999.\n
+  CACHESIZE 10000000\n
+  XDEF ${nx} LINEAR 0 1\n
+  YDEF ${ny} LINEAR 0 1\n
+  ZDEF ${outnz1} LEVELS ${outz}\n
+  TDEF ${nt} LINEAR 25FEB1998 ${deltatime}mn\n
+  VARS ${nvar}
+  ${varstring}\n
+  ENDVARS
+  "
+  echo -e ${string}>${outdir}/${type1}.ctl
+
+done
