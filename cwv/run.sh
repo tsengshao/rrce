9c80add4048d808152702e2e7b34e0628710307c
shao <shaoyu.ts@gmail.com>
2024-02-28 00:10:41 +0800

    feat(cwv): add lwp and iwp, combine three kind of water path in one output files

--- cwv/run.sh
+++ cwv/run.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/bash
-#SBATCH -J cwv     # Job name
+#SBATCH -J wp     # Job name
 #SBATCH -p all     # job partition
 #SBATCH -N 1       # Run all processes on a single node 
 #SBATCH -c 1        # cores per MPI rank
 #SBATCH -n 5       # Run a single task
-#SBATCH -w node01  # nodelist
-#SBATCH -o cwv.%j.out  # output file
+#SBATCH -w mogamd  # nodelist
+#SBATCH -o wp.%j.out  # output file
 
 
 source ~/.bashrc
 conda activate py311
 
-python -u cwv.py 0 &
-python -u cwv.py 1 &
-python -u cwv.py 2 &
-python -u cwv.py 3 &
-python -u cwv.py 4 &
-wait
+for i in $(seq 0 4);do
+  mpirun -np 5 python -u wp.py ${i}
+done
+
+#  python -u lwp.py 0 &
+#  python -u lwp.py 1 &
+#  python -u lwp.py 2 &
+#  python -u lwp.py 3 &
+#  python -u lwp.py 4 &
+#  wait
 
---
+++ cwv/wp.py
@@ -0,0 +1,67 @@
+import numpy as np
+import sys, os
+sys.path.insert(1,'../')
+import config
+from util.vvmLoader import VVMLoader, VVMGeoLoader
+import util.calculator as calc
+import util.tools as tools
+from util.dataWriter import DataWriter
+from mpi4py import MPI
+from datetime import datetime, timedelta
+
+comm = MPI.COMM_WORLD
+nproc = comm.Get_size()
+cpuid = comm.Get_rank()
+
+nexp = len(config.expList)
+iexp = int(sys.argv[1])
+
+nt = config.totalT[iexp]
+exp = config.expList[iexp]
+if (cpuid==0): print(exp, nt)
+
+outdir=config.dataPath+"/wp/"+exp+'/'
+
+vvmLoader = VVMLoader(f"{config.vvmPath}/{exp}/", subName=exp)
+thData = vvmLoader.loadThermoDynamic(0)
+nz, ny, nc = thData['qv'][0].shape
+xc, yc, zc = thData['xc'], thData['yc'], thData['zc']
+rho = vvmLoader.loadRHO()[:-1]
+
+idxTS, idxTE = tools.get_mpi_time_span(0, nt, cpuid, nproc)
+print(cpuid, idxTS, idxTE, idxTE-idxTS)
+dataWriter = DataWriter(outdir)
+
+for it in range(idxTS, idxTE):
+  timea = datetime.now()
+  thData = vvmLoader.loadThermoDynamic(it)
+  qv = thData['qv'][0] #3d, kg/kg
+  qc = thData['qc'][0] #3d, kg/kg
+  qr = thData['qr'][0] #3d, kg/kg
+  qi = thData['qi'][0] #3d, kg/kg
+  cwv = np.trapz((qv)*rho[:,np.newaxis,np.newaxis], x=zc, axis=0)
+  lwp = np.trapz((qc+qr)*rho[:,np.newaxis,np.newaxis], x=zc, axis=0)
+  iwp = np.trapz((qi)*rho[:,np.newaxis,np.newaxis], x=zc, axis=0)
+
+  if (cpuid==0):
+    print('it=',it, ', cwv_range=(%.2f, %.2f)'%(cwv.min(), cwv.max()))
+    print('it=',it, ', lwp_range=(%.2f, %.2f)'%(lwp.min(), lwp.max()))
+    print('it=',it, ', iwp_range=(%.2f, %.2f)'%(iwp.min(), iwp.max()))
+
+  dataWriter.toNC(f"wp-{it:06d}.nc", \
+    data=dict(
+      cwv=(["time", "yc", "xc"], cwv[np.newaxis, :, :]),
+      lwp=(["time", "yc", "xc"], lwp[np.newaxis, :, :]),
+      iwp=(["time", "yc", "xc"], iwp[np.newaxis, :, :]),
+    ),
+    coords=dict(
+      time=np.ones(1),
+      yc=(['yc'], yc),
+      xc=(['xc'], xc),
+    )
+  )
+  if (cpuid==0):
+    print('...',(datetime.now()-timea).total_seconds(),'sec')
+
+
+
