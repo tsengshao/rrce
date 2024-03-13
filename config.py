0fd9b79815b3a6a094e3b54c89bdfe071ad64180
shao <shaoyu.ts@gmail.com>
2024-03-13 19:53:51 +0800

    feat: add RRCE_3km_f05, config/cwv/horsf/restart

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 vvmPath  = '/data/C.shaoyu/rrce/vvm/'
 dataPath = '/data/C.shaoyu/rrce/data/'
 
-expList  = ['RCE_300K_3km_f0', 'RCE_300K_3km_f05', 'RRCE_3km_f00', 'RRCE_3km_f10', 'RRCE_3km_f15', 'RRCE_3km_f20']
-totalT   = [2137, 2030, 3654, 2286, 2161, 2138]
+expList  = ['RCE_300K_3km_f0', 'RCE_300K_3km_f05', 'RRCE_3km_f00', 'RRCE_3km_f05', 'RRCE_3km_f10', 'RRCE_3km_f15', 'RRCE_3km_f20']
+totalT   = [2137, 2030, 3654, 2765, 2286, 2161, 2138]
 
 def getExpDeltaT(exp):
   expheader=exp.split('_')[0]
--- horisf/run_py.sh
+++ horisf/run_py.sh
@@ -10,6 +10,7 @@
 source ~/.bashrc
 conda activate py311
 
-for i in $(seq 5 -1 0);do
+#for i in $(seq 5 -1 0);do
+for i in 4; do
   mpirun -np 40 python -u cal_sf_fft.py ${i}
 done
