8a894d134bc219b9e6ec81eed895fa5369ce481d
shao <shaoyu.ts@gmail.com>
2024-04-27 20:37:31 +0800

    feat(series): separate the series of streamfunction from main series.py

---
+++ series/run_msf.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+#SBATCH -J series     # Job name
+#SBATCH -p all     # job partition
+#SBATCH -N 1       # Run all processes on a single node 
+#SBATCH -c 1        # cores per MPI rank
+#SBATCH -n 20       # Run a single task
+#SBATCH -w mogamd  # nodelist
+#SBATCH -o series.%j.out  # output file
+
+
+source ~/.bashrc
+conda activate py311
+
+#for i in $(seq 0 5);do
+for i in 0 1 2 3 4;do
+mpirun -np 20 python -u series_msf.py ${i}
+done
+
+wait
--- series/series_msf.py
+++ series/series_msf.py
@@ -29,6 +29,7 @@ thData = vvmLoader.loadThermoDynamic(0)
 nz, ny, nx = thData['qv'][0].shape
 xc, yc, zc = thData['xc'], thData['yc'], thData['zc']
 rho = vvmLoader.loadRHO()[:-1]
+zz = vvmLoader.loadZZ()[:-1]
 
 idxTS, idxTE = tools.get_mpi_time_span(0, nt, cpuid, nproc)
 print(cpuid, idxTS, idxTE, idxTE-idxTS)
@@ -37,25 +38,17 @@ dataWriter = DataWriter(outdir)
 for it in range(idxTS, idxTE):
   print(exp, it)
   time = it*dtime #minutes
-  dyData = vvmLoader.loadDynamic(it)
-  u = dyData['u'][0, 0] # 2d, first layer
-  v = dyData['v'][0, 0] # 2d, first layer
+  hsf0km = np.fromfile(\
+             f'{config.dataPath}/horisf/{exp}/hrisf_0.00km_{it:06d}.dat',\
+             np.float32).reshape(ny,nx)
+  hsf275km = np.fromfile(\
+             f'{config.dataPath}/horisf/{exp}/hrisf_2.75km_{it:06d}.dat',\
+             np.float32).reshape(ny,nx)
 
-  nc  = Dataset(f'{config.dataPath}/wp/{exp}/wp-{it:06d}.nc', 'r')
-  cwv = nc.variables['cwv'][0,:,:]
-  lwp = nc.variables['lwp'][0,:,:]
-  iwp = nc.variables['iwp'][0,:,:]
-
-  dataWriter.toNC(f"series_{it:06d}.nc", \
+  dataWriter.toNC(f"series_hsf_{it:06d}.nc", \
     data=dict(
-      cwv_mean  = (["time"], [np.mean(cwv)]),
-      lwp_mean  = (["time"], [np.mean(lwp)]),
-      iwp_mean  = (["time"], [np.mean(iwp)]),
-      cwv_std   = (["time"], [np.std(cwv)]),
-      lwp_std   = (["time"], [np.std(lwp)]),
-      iwp_std   = (["time"], [np.std(iwp)]),
-      maxwind   = (["time"], [np.max((u**2+v**2)**0.5)]),
-      dryfrac   = (["time"], [np.sum(cwv<30)/cwv.size]),
+      sf000_max = (["time"], [np.nanmax(hsf0km)]),
+      sf275_max = (["time"], [np.nanmax(hsf275km)]),
     ),
     coords=dict(
       time=[it*dtime],
