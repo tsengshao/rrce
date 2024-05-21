de57bac47eaaa855ee9100b7f4a1adea5c87c4d7
shao <shaoyu.ts@gmail.com>
2024-05-21 22:13:31 +0800

    resubmit the convolution and horisf

--- horisf/run_py.sh
+++ horisf/run_py.sh
@@ -4,14 +4,14 @@
 #SBATCH -N 1       # Run all processes on a single node 
 #SBATCH -c 1        # cores per MPI rank
 #SBATCH -n 30       # Run a single task
-#SBATCH -w mogamd  # nodelist
-##SBATCH -w node01  # nodelist
+##SBATCH -w mogamd  # nodelist
+#SBATCH -w node01  # nodelist
 #SBATCH -o sfpy.%j.out  # output file
 
 source ~/.bashrc
 conda activate py311
 
-#for i in $(seq 5 -1 0);do
-for i in 14 15 16 17;do
+#for i in 7 ;do
+for i in $(seq 18 -1 0);do
   mpirun -np 30 python -u cal_sf_fft.py ${i}
 done
