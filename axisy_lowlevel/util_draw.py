1fe3a549d41c07022a2b902238ee6008fbc1e096
shao <shaoyu.ts@gmail.com>
2024-10-26 21:57:51 +0800

    fix(axisy_lowlevel): fixed util_draw.py missing

---
+++ axisy_lowlevel/util_draw.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+import matplotlib as mpl
+
+def set_figure_defalut():
+  plt.rcParams.update({'font.size':20,
+                       'axes.linewidth':2,
+                       'lines.linewidth':5})
+
+
+
+def set_black_background():
+  plt.rcParams.update({
+                       'axes.edgecolor': 'white',
+                       'axes.facecolor': 'black',
+                       'figure.edgecolor': 'black',
+                       'figure.facecolor': 'black',
+                       'text.color': 'white',
+                       'xtick.color': 'white',
+                       'ytick.color': 'white',
+                       'axes.labelcolor': 'white',
+                      })
+
+def get_cmap(name='colorful'):
+  if name=='pwo':
+    top = mpl.colormaps['Purples_r']
+    bottom = mpl.colormaps['Oranges']
+    newcolors = np.vstack((top(np.linspace(0.3, 1, 128)),\
+                           bottom(np.linspace(0, 0.7, 128)),\
+                         ))
+    newcmp = mpl.colors.ListedColormap(newcolors, name='OrangeBlue')
+  elif name=='colorful':
+    colors = ['w', '#00BFFF', '#3CB371', '#F0E68C', '#FFA500', '#FF6347']
+    nodes = np.linspace(0,1,len(colors))
+    newcmp = mpl.colors.LinearSegmentedColormap.from_list("cmap", list(zip(nodes, colors)))
+  return newcmp
+
