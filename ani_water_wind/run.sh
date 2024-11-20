e1f4e761a92171974bedfa2d8ef20d6ae4db3b61
shao <shaoyu.ts@gmail.com>
2024-11-20 23:25:41 +0800

    feat(ani_water): add new animation combination of cwv, olr, rain

---
+++ ani_water_wind/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#SBATCH -J draw     # Job name
+#SBATCH -p all     # job partition
+#SBATCH -N 1       # Run all processes on a single node 
+#SBATCH -c 1        # cores per MPI rank
+#SBATCH -n 34      # Run a single task
+#SBATCH -w mogamd  # nodelist
+#SBATCH -o draw.%j.out  # output file
+
+source ~/.bashrc
+mode="SAVEFIG"
+gs="draw.gs"
+
+for iexp in $(seq 2 18);do
+  for ts in 1 217;do
+    grads -blcx "run ${gs} ${iexp} -mode ${mode} -ts ${ts} -te ${ts}" &
+  done
+done
+wait
+
+for ts in $(seq 1 72 2161);do
+    te=$(echo "{ts}+1"|bc)
+    grads -blcx "run ${gs} 1 -mode ${mode} -ts ${ts} -te ${ts}" &
+done
+wait
